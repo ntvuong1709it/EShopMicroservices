b8c3dca1267a5417af218bbd477764bbdf80de6d
Vuong Nguyen <ntvuong1709it@gmail.com>
2026-02-23 20:40:27 +0700

    Add get product by id endpoint and handler

---
+++ src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdEndpoint.cs
@@ -0,0 +1,20 @@
+﻿using Catalog.API.Models;
+
+namespace Catalog.API.Products.GetProductById
+{
+    //public record GetProductByIdRequest(Guid id);
+    public record GetProductByIdResponse(Product Product);
+
+    public class GetProductByIdEndpoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapGet("/product/{id:guid}", async (Guid id, ISender sender) =>
+            {
+                var query = new GetProductByIdQuery(id);
+                var result = await sender.Send(query);
+                return Results.Ok(new GetProductByIdResponse(result.Product));
+            });
+        }
+    }
+}
---
+++ src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdQueryHandler.cs
@@ -0,0 +1,26 @@
+﻿using BuildingBlocks.CQRS;
+using Catalog.API.Exceptions;
+using Catalog.API.Models;
+using Marten;
+
+namespace Catalog.API.Products.GetProductById
+{
+    public record GetProductByIdResult(Product Product);
+    public record GetProductByIdQuery(Guid Id) : IQuery<GetProductByIdResult>;
+
+    public class GetProductByIdQueryHandler(IDocumentSession session,
+        ILogger<GetProductByIdQueryHandler> logger)
+        : IQueryHandler<GetProductByIdQuery, GetProductByIdResult>
+    {
+        public async Task<GetProductByIdResult> Handle(GetProductByIdQuery request, CancellationToken cancellationToken)
+        {
+            logger.LogInformation("GetProductByIdQueryHandler.Handle called with {@Request}", request);
+            var product = await session.LoadAsync<Product>(request.Id, cancellationToken);
+            if (product == null)
+            {
+                throw new ProductNotFoundException();
+            }
+            return new GetProductByIdResult(product);
+        }
+    }
+}
